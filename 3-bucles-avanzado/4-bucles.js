67b73286079a8f3ca1bcdc3cd2db22027e655b19
GuiBoCo <guiboco18@gmail.com>
2019-05-16 13:50:43 +0200

    bucles avanzados/4

--- 3-bucles-avanzado/4-bucles.js
+++ 3-bucles-avanzado/4-bucles.js
@@ -10,7 +10,10 @@ let frase = Math.random() > 0.5 ? ['unite', 'states', 'america'] : ['aeropuertos
 console.log('El array frase es: ', frase);
 // Aquí tu código
 
-var acronimo = "string";
+var acronimo = "";
+for (var i = 0; i < frase.length ; i++) {
+    acronimo+= frase[i].charAt(0).toUpperCase();
+        }
 
 // Hasta aquí
 console.log('El string acronimo es: ', acronimo);
